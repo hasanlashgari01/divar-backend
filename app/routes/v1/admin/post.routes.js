a8487f742d388dfddd8032aa2f2465d7772ce47a
Hasan Lashgari <hasanlashgari01@gmail.com>
2024-01-02 23:57:04 +0330

    post method change to get

--- app/routes/v1/admin/post.routes.js
+++ app/routes/v1/admin/post.routes.js
@@ -6,7 +6,7 @@ const controller = require("../../../controllers/v1/admin/post.controller");
 
 router.use(authMiddleware, isAdminMiddleware);
 router.route("/").get(controller.getAll);
-router.route("/publish/:id").put(controller.publishedByAdmin);
-router.route("/draft/:id").put(controller.draftedByAdmin);
+router.route("/publish/:id").get(controller.publishedByAdmin);
+router.route("/draft/:id").get(controller.draftedByAdmin);
 
 module.exports = { postAdminRouter: router };
