d902e3111f3975d318c0d17cb950f540916290da
Hasan Lashgari <hasanlashgari01@gmail.com>
2024-01-10 02:08:28 +0330

    get all users banned

--- app/controllers/v2/admin/user.controller.js
+++ app/controllers/v2/admin/user.controller.js
@@ -1,9 +1,10 @@
+const { BanUserModel } = require("../../../models/banUser");
 const { UserModel } = require("../../../models/user");
 
 exports.getAllUsers = async (req, res, next) => {
     try {
         const { sort: sortTitle } = req.query;
-        const users = await UserModel.find({}, { password: 0, __v: 0, updatedAt: 0 })
+        const users = await UserModel.find({ role: "USER" }, { password: 0, __v: 0, updatedAt: 0 })
             .sort({ [sortTitle]: 1 })
             .lean();
         const filteredUsers = users.filter(obj => obj.username !== req.user.username);
@@ -12,6 +13,20 @@ exports.getAllUsers = async (req, res, next) => {
             count: filteredUsers.length,
             users: filteredUsers,
         });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.getAllBanUsers = async (req, res, next) => {
+    try {
+        const { sort: sortTitle } = req.query;
+        const users = await BanUserModel.find({}, { password: 0, __v: 0, updatedAt: 0 })
+            .sort({ [sortTitle]: 1 })
+            .lean();
+        const filteredUsers = users.filter(obj => obj.username !== req.user.username);
+
+        return res.json({ users: filteredUsers });
     } catch (error) {
         next(error);
     }
--- app/routes/v2/admin/user.routes.js
+++ app/routes/v2/admin/user.routes.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const controller = require("../../../controllers/v2/admin/user.controller");
 
 router.route("/").get(controller.getAllUsers);
+router.route("/banned").get(controller.getAllBanUsers);
 
 module.exports = { userAdminRouter: router };
